91f42d17b2103605b9e25dd89d19fae182aadd3b
Steven Gort <steven.gort@ictu.nl>
2025-07-01 12:35:24 +0200

    added name and version

--- operaton-dmn-plugin.php
+++ operaton-dmn-plugin.php
@@ -3,8 +3,8 @@
  * Plugin Name: Operaton DMN Evaluator
  * Plugin URI: https://github.com/yourorg/operaton-dmn-evaluator
  * Description: WordPress plugin to integrate Gravity Forms with Operaton DMN decision tables for dynamic form evaluations.
- * Version: 1.0.0
- * Author: Your Name
+ * Version: 1.0.0-beta.1
+ * Author: Steven Gort
  * License: GPL v2 or later
  * Text Domain: operaton-dmn
  */
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('OPERATON_DMN_VERSION', '1.0.0');
+define('OPERATON_DMN_VERSION', '1.0.0-beta.1');
 define('OPERATON_DMN_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('OPERATON_DMN_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
